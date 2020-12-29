e45c3ba2a18a24f38b6d00952eb45696fc5d8aae
Joudrier Kevin <kev.joudrier@gmail.com>
2020-12-29 15:39:58 +0100

    Test full wall on the map.

--- Game/Map/Map.js
+++ Game/Map/Map.js
@@ -69,7 +69,7 @@ export class Map {
      **/
     build() {
         this.map = this.generator.generatedEmptyMap();
-        this.map = this.generator.generatedWallInMap(20);
+        this.map = this.generator.generatedWallInMap(100);
     }
 
     /**

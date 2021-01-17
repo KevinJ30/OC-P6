a51705a159ccc47a97fd767c705fc6c7cf059635
Joudrier Kevin <kev.joudrier@gmail.com>
2021-01-17 23:28:01 +0100

    Create element with javascript.

--- Game/Views/HUDView.js
+++ Game/Views/HUDView.js
@@ -3,14 +3,29 @@ export class HUDView {
     constructor() {
         this.HUDContainer = $('.js-HUD');
 
-        this.HUDContainerPlayerOne = $('<div></div>').addClass('js-hud__player-1').append('<p></p>');
-        this.HUDContainerPlayerTwo = $('<div></div>').addClass('js-hud__player-2').append('<p></p>');
+        /**this.HUDContainerPlayerOne = $('<div></div>').addClass('js-player-hud-1');
+        this.HUDContainerPlayerTwo = $('<div></div>').addClass('js-player-hud-2');
+
+        this.playerOneNameElement = $('<p><p>').addClass('js-player1-name');
+        //this.playerTwoNameElement = $('<p><p>').addClass('js-player2-name');
+
+        /**this.HUDContainerPlayerOne.append(this.playerOneNameElement)
+        this.HUDContainerPlayerTwo.append(this.playerTwoNameElement)
 
         this.HUDContainer.append(this.HUDContainerPlayerOne);
         this.HUDContainer.append(this.HUDContainerPlayerTwo);
+        this.HUDContainerPlayerOne.append(this.playerOneNameElement);
+        this.HUDContainerPlayerTwo.append(this.playerTwoNameElement);
 
-        this.playerOneNameElement = $('.js-hud__player-1').children()
-        this.playerTwoNameElement = $('.js-hud__player-2').children()
+        this.HUDContainer.append(this.HUDContainerPlayerOne);**/
+
+        this.HUDContainerPlayerOne = $('<div></div>').addClass('js-player-hud-1').append('<p></p>');
+        this.HUDContainerPlayerTwo = $('<div></div>').addClass('js-player-hud-2').append('<p></p>');
+        this.playerOneNameElement = this.HUDContainerPlayerOne.children().addClass('js-player1-name')
+        this.playerTwoNameElement = this.HUDContainerPlayerTwo.children().addClass('js-player2-name')
+
+        this.HUDContainer.append(this.HUDContainerPlayerOne);
+        this.HUDContainer.append(this.HUDContainerPlayerTwo);
     }
 
     updateDisplay(gameStore) {

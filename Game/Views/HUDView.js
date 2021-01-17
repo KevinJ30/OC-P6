ee67489d071b955993159dc1a8588566fa3c0a1f
Joudrier Kevin <kev.joudrier@gmail.com>
2021-01-17 23:52:29 +0100

    Connect progress into the player info.

--- Game/Views/HUDView.js
+++ Game/Views/HUDView.js
@@ -21,8 +21,13 @@ export class HUDView {
 
         this.HUDContainerPlayerOne = $('<div></div>').addClass('js-player-hud-1').append('<p></p>');
         this.HUDContainerPlayerTwo = $('<div></div>').addClass('js-player-hud-2').append('<p></p>');
-        this.playerOneNameElement = this.HUDContainerPlayerOne.children().addClass('js-player1-name')
-        this.playerTwoNameElement = this.HUDContainerPlayerTwo.children().addClass('js-player2-name')
+        this.playerOneNameElement = this.HUDContainerPlayerOne.children().addClass('js-player1-name');
+        this.playerTwoNameElement = this.HUDContainerPlayerTwo.children().addClass('js-player2-name');
+        this.HUDContainerPlayerOne.append('<progress class="health-bar js-hud__health-player1" value="0" max="100"></progress>')
+        this.HUDContainerPlayerTwo.append('<progress class="health-bar js-hud__health-player1" value="0" max="100"></progress>')
+
+        this.playerOneProgressElement = this.HUDContainerPlayerOne.children().last();
+        this.playerTwoProgressElement = this.HUDContainerPlayerTwo.children().last();
 
         this.HUDContainer.append(this.HUDContainerPlayerOne);
         this.HUDContainer.append(this.HUDContainerPlayerTwo);
@@ -31,5 +36,9 @@ export class HUDView {
     updateDisplay(gameStore) {
         this.playerOneNameElement.text(gameStore.getState().players[0].model.username + ' ' + gameStore.getState().players[0].model.health);
         this.playerTwoNameElement.text(gameStore.getState().players[1].model.username + ' ' + gameStore.getState().players[1].model.health);
+
+        // Connect value progress bar
+        this.playerOneProgressElement.val(gameStore.getState().players[0].model.health);
+        this.playerTwoProgressElement.val(gameStore.getState().players[1].model.health);
     }
 }

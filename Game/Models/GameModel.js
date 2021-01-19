c8b146d0b07abd9124f77f9f00fb4fb30b248e63
Joudrier Kevin <kev.joudrier@gmail.com>
2021-01-19 14:29:38 +0100

    Create GameModel class.

--- Game/Models/GameModel.js
+++ Game/Models/GameModel.js
@@ -3,7 +3,14 @@ import {Observer} from "../Observer/Observer.js";
 export class GameModel extends Observer {
 
     constructor() {
+        super();
 
+        this.map = null;
+        this.players = [];
+        this.playerSelected = 0;
+        this.isStarted = false;
+        this.isFight = false;
     }
+
 
 }

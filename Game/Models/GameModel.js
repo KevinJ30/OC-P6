5213b2044375735379afc2ab15d9a23c333e7313
Joudrier Kevin <kev.joudrier@gmail.com>
2021-01-19 17:24:13 +0100

    Create property and method of the GameModel

--- Game/Models/GameModel.js
+++ Game/Models/GameModel.js
@@ -1,5 +1,15 @@
 import {Observer} from "../Observer/Observer.js";
 
+/**
+ * Manage data of the GameController
+ * class GameModel
+ *
+ * @property {Map} map
+ * @property {Array} players
+ * @property {number} playerSelected
+ * @property {boolean} isStarted
+ * @property {boolean} isFight
+ **/
 export class GameModel extends Observer {
 
     constructor() {
@@ -12,5 +22,68 @@ export class GameModel extends Observer {
         this.isFight = false;
     }
 
+    /**
+     *
+     * @param {{view: PlayerView, model: PlayerModel}} player
+     **/
+    addPlayer(player) {
+        this.players.push(player);
+    }
 
+    /**
+     *
+     * @param {number} index
+     * @returns {Object}
+     **/
+    getPlayerIndex(index) {
+        return this.players[index];
+    }
+
+    /**
+     * Return array of the player
+     * @returns {Array}
+     **/
+    getPlayers() {
+        return this.players;
+    }
+
+    /**
+     * Return player selected
+     * @returns {Object}
+     **/
+    getPlayerSelected() {
+        return this.players[this.playerSelected];
+    }
+
+    /**
+     * Return the player who is not selected
+     * @returns {Object}
+     **/
+    getPlayerNotSelected(){
+        return this.players[this.playerSelected ? 0 : 1];
+    }
+
+    /**
+     * return number of the player
+     * @returns {number}
+     **/
+    countPlayer() {
+        return this.players.length;
+    }
+
+    /**
+     * Return the game started
+     * @returns {boolean}
+     **/
+    getIsStarted() {
+        return this.isStarted;
+    }
+
+    /**
+     * Return if the game is in combat stage
+     * @returns {boolean}
+     */
+    getIsFight() {
+        return this.isFight;
+    }
 }

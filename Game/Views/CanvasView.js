697b80a7022005693fa4d202b744b8dacfaa6d87
Kevin Joudrier <kev.joudrier@gmail.com>
2021-02-24 23:27:46 +0100

    Add all comments CanvasView & GameOverView

--- Game/Views/CanvasView.js
+++ Game/Views/CanvasView.js
@@ -1,13 +1,46 @@
+/**
+ * Classe CanvasView créer par Joudrier Kevin
+ * 
+ * @property {CanvasRenderingContext2D} ctx : Canvas HTML
+ **/
 export class CanvasView {
 
+    /**
+     * @param {CanvasRenderingContext2D} canvasHtml 
+     **/
     constructor(canvasHtml) {
         this.ctx = $('#screen')[0].getContext('2d');
     }
 
+    /**
+     * Dessine sur le canvas HTML
+     * 
+     * @param {HTMLImageElement} spriteSheet
+     * @param {number} sourceX 
+     * @param {number} sourceY 
+     * @param {number} sourceW 
+     * @param {number} sourceH 
+     * @param {number} destinationX 
+     * @param {number} destinationY 
+     * @param {number} destinationW 
+     * @param {number} destinationH
+     * 
+     * @return {void}
+     **/
     draw(spriteSheet, sourceX, sourceY, sourceW, sourceH, destinationX, destinationY, destinationW, destinationH){
         this.ctx.drawImage(spriteSheet, sourceX, sourceY, sourceW, sourceH, destinationX, destinationY, destinationW, destinationH);
     }
 
+    /**
+     * Efface la zone du canvas définit
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} w 
+     * @param {number} h 
+     * 
+     * @return {void}
+     **/
     resetCanvas(x, y, w, h) {
         this.ctx.clearRect(x, y, w, h);
     }
--- Game/Views/GameOverView.js
+++ Game/Views/GameOverView.js
@@ -1,24 +1,58 @@
+/**
+ * Classe GameOverView créer par Joudrier Kevin
+ * 
+ * @property {HTMLElement} HUDContainer : Elément qui tous les éléments HTML du HUD
+ * @property {HTMLElement} usernameElement : Elément qui contient le nom du joueur qui a perdu
+ * @property {HTMLElement} restartButtonElement : Elément bouton restart
+ **/
 export class GameOverView {
 
+    /**
+     * @property {string} CLASS_HIDDEN : Nom de la class qui permet de masquer les éléments
+     **/
     static CLASS_HIDDEN = 'hidden'
 
+    /**
+     * Constructeur.
+     **/
     constructor() {
+        // Séléction des éléments HTML
         this.HUDContainer = $('.HUD__game-over');
-        this.HUDContainer.addClass('hidden');
         this.usernameElement = $('.js-usernameElement');
+
+        // Ajouts des classes par défaut
+        this.HUDContainer.addClass('hidden');
+
         this.restartButtonElement = this.createRestartButtonElement();
 
+        // Ajouts des éléments dans le conteneur HUD
         this.HUDContainer.append(this.restartButtonElement);
     }
 
+    /**
+     * Construit le bouton restart du menu game over
+     * 
+     * @return {HTMLElement} Bouton restart
+     **/
     createRestartButtonElement() {
         return $('<button class="btn btn-primary">Relancer une partie</button>')
     }
 
+    /**
+     * Affiche ou masque le menu game over
+     * 
+     * @return {void}
+     **/
     toggleDisplay() {
         this.HUDContainer.toggleClass(GameOverView.CLASS_HIDDEN);
     }
 
+    /**
+     * Lie le bouton restart a la méthode handleButtonRestart du controller
+     * 
+     * @param {function} handler 
+     * @return {void}
+     **/
     bindButtonRestart(handler) {
         this.restartButtonElement.click((event) => {
             event.preventDefault();
@@ -26,6 +60,12 @@ export class GameOverView {
         })
     }
 
+    /**
+     * Met à jour l'élement username
+     * 
+     * @param {string} playerDead 
+     * @return {void}
+     **/
     updateUsername(playerDead) {
         this.usernameElement.text(playerDead);
     }

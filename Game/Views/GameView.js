f6ce4597ae9b403258e52fa91999f2e26e41b2f3
Kevin Joudrier <kev.joudrier@gmail.com>
2021-02-24 23:31:59 +0100

    Add all comments GameView & Spritesheet.

--- Game/Views/GameView.js
+++ Game/Views/GameView.js
@@ -1,5 +1,10 @@
 import {CanvasView} from "../Views/CanvasView.js";
-
+ 
+/**
+ * Classes GameView créer par Joudrier Kevin
+ * 
+ * @property {HTMLImageElement} background : Image d'arrière du plateau de jeu
+ **/
 export class GameView extends CanvasView {
 
     constructor() {
--- Game/Views/Spritesheet.js
+++ Game/Views/Spritesheet.js
@@ -1,4 +1,9 @@
-export class Spritesheet{
+/**
+ * Classes Spritesheet créer par Joudrier Kevin
+ * 
+ * @property {HTMLImageElement} image : Image représentant la sprite
+ **/
+export class Spritesheet {
 
     constructor(src) {
         this.image = new Image();

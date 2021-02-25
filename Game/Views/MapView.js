ad8dec4aa92b0271db831561dc6f61e2a6740203
Joudrier Kevin <kev.joudrier@gmail.com>
2021-02-25 14:25:35 +0100

    Add all comments MapView class.

--- Game/Views/MapView.js
+++ Game/Views/MapView.js
@@ -1,12 +1,21 @@
 import {Config} from '../config/Config.js';
 import {CanvasView} from './CanvasView.js';
 
+/**
+ * classe MapView créer par Joudrier Kévin
+ * @extends {CanvasView}
+ **/
 export class MapView extends CanvasView {
 
     constructor() {
         super();
     }
 
+    /**
+     * Ajoute une grile par dessu la map
+     * 
+     * @return {void}
+     **/
     addGridToMap() {
         this.context.beginPath();
         this.context.moveTo(75, 50);
@@ -16,13 +25,12 @@ export class MapView extends CanvasView {
     }
 
     /**
-     * Draw map
+     * Affiche la map sur le canvas
      *
-     * @param {CanvasImageSource} spriteSheet
-     * @param {CanvasRenderingContext2D} context
-     * @param {Array} map
-     * @param {number} maxTileY
-     * @param {number} maxTileX
+     * @param {CanvasImageSource} spriteSheet : image contenant les sprites
+     * @param {Array} map : Tableaux contenant les informations de la map
+     * @param {number} maxTileX : largeur maximum de la map en nombre de Tile
+     * @param {number} maxTileY : Hauteur maximum de la map en nombre de Tile
      **/
     drawMap(spriteSheet, map, maxTileX, maxTileY) {
         const tileSize = Config.TILE_SIZE;
@@ -33,12 +41,19 @@ export class MapView extends CanvasView {
                     x : j * tileSize,
                     y : i * tileSize
                 }
-
+                
+                // Numéro de la Tile
                 let a = map[i][j];
-
+                
+                /**
+                 * Calcule la position de Tile sur le spritesheet en X et Y
+                 **/
                 const sourceX = Math.floor(a % Config.MAX_NUMBER_TILESET) * tileSize;
                 const sourceY = Math.floor((a / Config.MAX_NUMBER_TILESET)) *  tileSize;
-
+                
+                /**
+                 * Affiche la Tile sur le canvas
+                 **/
                 this.draw(spriteSheet, sourceX, sourceY, tileSize, tileSize, positionTile.x, positionTile.y, tileSize, tileSize);
                 this.ctx.strokeStyle = "rgba(0, 0, 0, 0.7)";
                 this.ctx.strokeRect(positionTile.x, positionTile.y, tileSize, tileSize);
@@ -47,12 +62,12 @@ export class MapView extends CanvasView {
     }
 
     /**
-     * Add event element of the map
+     * Ajoutes les éléments d'événement sur la map (Trésor de guerre)
      *
-     * @param {CanvasImageSource} spriteSheet
-     * @param {Array} mapEvents
-     * @param {number} maxTileX
-     * @param {number} maxTileY
+     * @param {CanvasImageSource} spriteSheet : Image contenant toutes les Tile
+     * @param {Array} mapEvents : Tableau contenant le placement des événement sur la map
+     * @param {number} maxTileX : largeur maximum de la map en nombre de Tile
+     * @param {number} maxTileY : Hauteur maximun de la map en nombre de Tile
      **/
     drawEvents(spriteSheet, mapEvents, maxTileX, maxTileY) {
         const tileSize = Config.TILE_SIZE;
@@ -66,6 +81,7 @@ export class MapView extends CanvasView {
                     }
 
                     const a = Config.BOX_TILE;
+
                     const sourceX = Math.floor(a % Config.MAX_NUMBER_TILESET) * tileSize;
                     const sourceY = Math.floor((a / Config.MAX_NUMBER_TILESET)) *  tileSize;
 

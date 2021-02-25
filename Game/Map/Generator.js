3a50779f701555fd9e4534c72b483293966d099c
Joudrier Kevin <kev.joudrier@gmail.com>
2021-02-25 23:03:20 +0100

    Add all comments on the Generator class.

--- Game/Map/Generator.js
+++ Game/Map/Generator.js
@@ -4,16 +4,17 @@ import {Drunken} from "./Drunken.js";
 import {DrunkenWalk} from "./DrunkenWalk.js"
 
 /**
- * Created by Joudrier Kevin
+ * Classe Generator créer par Joudrier Kevin
  **/
 export class Generator {
+
     /**
      * Constructor.
      *
-     * @param maxTileX
-     * @param maxTileY
-     * @param blankTileNumber
-     * @param wallTileNumber
+     * @param maxTileX : Largeur maximum de la map en nombre de Tile
+     * @param maxTileY : Hauteur Maximum de la map en nombre de Tile
+     * @param blankTileNumber : Numéro de la tile qui correspond à rien
+     * @param wallTileNumber : Numéro de la tile qui correspond a un mur
      **/
     constructor(maxTileX, maxTileY, blankTileNumber, wallTileNumber) {
         this.drunkenWalk = new DrunkenWalk(3);
@@ -29,22 +30,20 @@ export class Generator {
     }
 
     /**
-     * Generate empty map
+     * Créer une map vierge qu'avec des murs
      *
      * @returns {Array} map
      **/
     generatedEmptyMap() {
-        // créer un map vide
         for(let i = 0; i < this.maxTileY; i++) {
             let row = [];
             let colsCollision = [];
 
             for(let j = 0; j < this.maxTileX; j++) {
-                // genere un nombre aleatoire pour ajouter une tuile d'eau ou d'herbe
-                let randomWater = Utils.randomNumber(0, 3);
                 row.push(Config.WALL_TILE);
                 colsCollision.push(0);
             }
+
             this.map.push(row);
             this.mapCollision.push(colsCollision);
         }
@@ -52,34 +51,6 @@ export class Generator {
         return this.map;
     }
 
-    /**
-     * Generate wall map
-     *
-     * @param {number} fillPourcentage : pourcent wall generate
-     * @returns {Array} map
-     **/
-    generatedWallInMap(fillPourcentage = 30) {
-        fillPourcentage = fillPourcentage === undefined ? 0 : fillPourcentage;
-        let wallNumber = (this.maxTileY * this.maxTileX) * fillPourcentage / 100;
-        let wallPosition = [];
-        for(let i = 0; i < wallNumber; i++) {
-            let randomX = Utils.randomNumber(0, this.maxTileX);
-            let randomY = Utils.randomNumber(0, this.maxTileY);
-            
-            // if exists wall in the case then change random number
-            while(this.map[randomY][randomX] === 32) {
-                randomX = Utils.randomNumber(0, this.maxTileX);
-                randomY = Utils.randomNumber(0, this.maxTileY);
-            }
-            
-
-            this.map[randomY][randomX] = this.WALL_TILE;
-            wallPosition.push({x: randomX, y: randomY});
-            this.mapCollision[randomY][randomX] = 1;
-        }
-
-        return this.map;
-    }
 
     /**
      * ALGO : DRUNKEN WALK
@@ -100,16 +71,23 @@ export class Generator {
      **/
 
     /**
-     * Generated ground on the map
+     * Créer le sol de la map
      * 
-     * @param {number} coverage : percentage coverage on the map 
+     * @return {}
      **/
-    generateGround(coverage) {
-        // Number of drunk that are used on the map   
+    generateGround() {
+        // Nombre de drunk créé sur la map   
         this.drunkenWalk.init();
+
+        // Retourne la nouvelle générer quand on démarrer les drunks
         return this.drunkenWalk.startDrunk(this.map);
     }
 
+    /**
+     * Créer la map de collision avec le murs qu'il reste sur la map
+     * 
+     * @return {Array} mapCollision : Map de collision
+     **/
     generateMapCollision() {
         let map = this.map;
 
@@ -129,90 +107,5 @@ export class Generator {
         }
 
         return mapCollision;
-    }
-
-    addStand(number) {
-        // Generate aléatoire
-        for(let i = 0; i < number; i++) {
-            let coordinates = this.generateCoordinates(2);
-
-            while(this.collisionStand(coordinates.x, coordinates.y)) {
-                coordinates = this.generateCoordinates(2);
-            }
-
-            this.generateStand(coordinates.x, coordinates.y);
-        }
-
-        return this.map;
-    }
-
-    generateStand(startX, startY) {
-        this.map[startY][startX] = Config.STAND_TILES[0];
-        this.map[startY][startX + 1] = Config.STAND_TILES[1];
-        this.map[startY + 1][startX] = Config.STAND_TILES[2];
-        this.map[startY + 1][startX + 1] = Config.STAND_TILES[3];
-    }
-
-    collisionStand(startX, startY) {
-        if(this.detectCollision(this.map[startY][startX]) || this.detectCollision(this.map[startY][startX + 1])
-            || this.detectCollision(this.map[startY + 1][startX]) || this.detectCollision(this.map[startY + 1][startX + 1])
-        ){
-            return true;
-        }
-
-        return false;
-    }
-
-    detectCollision(tileNumberDetect) {
-        return tileNumberDetect === 14 || tileNumberDetect === 15 || tileNumberDetect === 30 || tileNumberDetect === 31;
-    }
-
-    /**
-     * Return new coordinates
-     * @param {number} offset
-     * @returns {{x: number, y: number}} : Vector 2D
-     **/
-    generateCoordinates(offset = 0) {
-        return {
-            x : Utils.randomNumber(0, Config.MAP_MAX_X - offset),
-            y : Utils.randomNumber(0, Config.MAP_MAX_Y - offset)
-        };
-    }
-
-    /**
-     * Return collition map
-     *
-     * @returns {Array} mapCollision
-     **/
-    getCollisionMap() {
-        return this.mapCollision;
-    }
-
-    addWater(bulk) {
-        let coordinates = this.generateCoordinates();
-        let limit = bulk / 2;
-
-        // console.log(this.maxTileX - coordinates.x);
-        // console.log(this.maxTileY - coordinates.y);
-
-        for(let i = 0; i < limit; i++) {
-            for(let j = 0; j < limit; j++) {
-                if(this.maxTileX - coordinates.x >= bulk && this.maxTileY - coordinates.y >= bulk) {
-                    this.map[coordinates.y + i][coordinates.x + j] = Config.WATER_TILE;
-                }
-                else if(this.maxTileX - coordinates.x < bulk && this.maxTileY - coordinates.y < bulk){
-                    this.map[coordinates.y - i][coordinates.x - j] = Config.WATER_TILE;
-                }
-                else if(this.maxTileX - coordinates.x >= bulk && this.maxTileY - coordinates.y < bulk){
-                    this.map[coordinates.y - i][coordinates.x + j] = Config.WATER_TILE;
-                }
-                else {
-                    this.map[coordinates.y + i][coordinates.x - j] = Config.WATER_TILE;
-                }
-
-            }
-        }
-
-        return this.map;
     }
 }
--- Game/Utils.js
+++ Game/Utils.js
@@ -1,5 +1,16 @@
+/**
+ * Classe Utils créer par Joudrier Kevin
+ **/
 export class Utils {
 
+    /**
+     * Retourne un nombre aléatoire dans un interval donné
+     * 
+     * @param {number} min : Interval minimum
+     * @param {number} max  : Interval maximum
+     * 
+     * @return {number} : Nombre aléatoire
+     **/
     static randomNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }

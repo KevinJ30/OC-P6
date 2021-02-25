7f4d36a68113747e361a27a55a7d91f6c13b807f
Joudrier Kevin <kev.joudrier@gmail.com>
2021-02-25 23:25:28 +0100

    Add all comments on the MenuController class.

--- Game/Controllers/MenuController.js
+++ Game/Controllers/MenuController.js
@@ -1,23 +1,41 @@
+/**
+ * Classe MenuController créer par Joudrier Kevin
+ * 
+ * @property {MenuView} menuView
+ * @property {GameModel} gameModel : Modèle du jeu
+ * @property {EventManager} eventManager : Liste des événement
+ **/
 export class MenuController {
     
-    constructor (eventManager, gameModel, view) {
-        this.view = view;
+    /**
+     * @param {*} eventManager 
+     * @param {*} gameModel 
+     * @param {*} menuView 
+     **/
+    constructor (eventManager, gameModel, menuView) {
+        this.menuView = menuView;
         this.gameModel = gameModel;
         this.eventManager = eventManager;
         this.bindEventHandler();
     }
 
+    /**
+     * Lie l'évément du bouton startGame
+     **/
     bindEventHandler() {
-        this.view.bindButtonStartGame(this.handleButtonStartGame);
+        this.menuView.bindButtonStartGame(this.handleButtonStartGame);
     }
 
+    /**
+     * Evénement du bouton start game
+     **/
     handleButtonStartGame = () => {
-        this.gameModel.players[0].model.username = this.view.fieldPlayerOneNameElement.val();
-        this.gameModel.players[1].model.username = this.view.fieldPlayerTwoNameElement.val();
+        this.gameModel.players[0].model.username = this.menuView.fieldPlayerOneNameElement.val();
+        this.gameModel.players[1].model.username = this.menuView.fieldPlayerTwoNameElement.val();
         this.gameModel.isStarted = true;
         this.gameModel.notify();
 
-        this.view.displayMenu();
+        this.menuView.displayMenu();
 
         this.eventManager.trigger('game.startGameEvent');
     }

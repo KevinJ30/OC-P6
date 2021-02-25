c6b8b6828d224ba35239746d09b7aab1d7f1f3ea
Joudrier Kevin <kev.joudrier@gmail.com>
2021-02-25 23:43:53 +0100

    Add all comments on the GameOVerController class .

--- Game/Controllers/GameOverController.js
+++ Game/Controllers/GameOverController.js
@@ -1,5 +1,13 @@
+/**
+ * Classe GameOverController créer par Joudrier Kevin
+ **/
 export class GameOverController {
 
+    /**
+     * @param {EventManager} eventManager 
+     * @param {GameOverModel} gameOverModel 
+     * @param {GameOverView} gameOverView 
+     **/
     constructor(eventManager, gameOverModel, gameOverView) {
         this.eventManager = eventManager;
         this.gameOverModel = gameOverModel;
@@ -11,17 +19,34 @@ export class GameOverController {
         this.eventManager.attach('game.restartGame', this.handleDisplay, 0);
     }
 
+    /**
+     * Bind la classe sur les méthode des événements
+     * 
+     * @return {void}
+     **/
     bindingMethodOfClass() {
         this.handleDisplay = this.handleDisplay.bind(this);
         this.handleButtonRestart = this.handleButtonRestart.bind(this);
         this.gameOverView.bindButtonRestart(this.handleButtonRestart);
     }
 
+    /**
+     * Evénement display
+     * Déclenché par l'event manager
+     * 
+     * @param {string} playerDead : Nom du joueur qui est mort 
+     **/
     handleDisplay(playerDead) {
         this.gameOverView.toggleDisplay();
         this.gameOverView.updateUsername(playerDead)
     }
 
+    /**
+     * Evénement bouton redémarrage de la partie
+     * Déclenché par la vue GameOverView
+     * 
+     * @return {void}
+     **/
     handleButtonRestart() {
         this.eventManager.trigger('game.restartGame');
     }

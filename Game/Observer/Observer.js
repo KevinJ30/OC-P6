c0d17c60e625b63fee33fd25186762bd8a3dff44
Joudrier Kevin <kev.joudrier@gmail.com>
2021-02-25 15:05:13 +0100

    Add comments on the Observer class.

--- Game/Observer/Observer.js
+++ Game/Observer/Observer.js
@@ -1,17 +1,39 @@
+/**
+ * classe Observer créer par Joudrier Kevin
+ * 
+ * @property {array} observers : Contient toutes les observers
+ **/
 export class Observer {
 
     constructor() {
         this.observers = [];
     }
 
+    /**
+     * Souscrit a un observer
+     * 
+     * @param {CallableFunction} observer 
+     * @return {void}
+     **/
     subscribe(observer) {
         this.observers.push(observer);
     }
 
+    /**
+     * Supprime la souscription a un observer
+     * 
+     * @param {CallableFunction} observer
+     * @return  {void}
+     **/
     unsubscribe(observer) {
         this.observers = this.observers.filter(subscriber => subscriber !== observer);
     }
 
+    /**
+     * Avertie des modifications sur un observer
+     * 
+     * @param {array} data 
+     **/
     notify(data) {
         this.observers.forEach(observer => observer(data));
     }

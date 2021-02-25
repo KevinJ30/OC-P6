d475a9406e0789c267cf6e20601e995870d8dcac
Joudrier Kevin <kev.joudrier@gmail.com>
2021-02-25 22:33:39 +0100

    Add all comments on the WeaponModel class.

--- Game/Models/WeaponModel.js
+++ Game/Models/WeaponModel.js
@@ -1,6 +1,13 @@
+/**
+ * class WeaponModel créer par Joudrier Kevin
+ * 
+ * @property {string} name : Nom de l'arme
+ * @property {number} damage : Dégats de l'arme
+ **/
 export class WeaponModel {
     /**
-     * @param {number} damage
+     * @param {string} name : Nom de l'arme
+     * @param {number} damage : Dégats de l'arme
      **/
     constructor(name, damage) {
         this.name = name;
